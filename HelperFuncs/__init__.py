03ad1a79a560ffd22734254939ca6a31828dc6f9
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-04-06 20:05:59 -0700

    Small style and bug fixes

--- HelperFuncs/__init__.py
+++ HelperFuncs/__init__.py
@@ -10,7 +10,7 @@ es_url = 'https://{}:{}@{}/{}/'.format(ES_AUTH_TUPLE[0],
                                        ES_INDEX)
 
 
-def get_must_bool_filter_query(query_dict):
+def must_bool_filter_query(query_dict):
     """
     Takes a dict of paramters and returns a matching boolean filter
     :param dict query_dict:
@@ -27,15 +27,16 @@ def get_must_bool_filter_query(query_dict):
     }
 
 
-def get_new_elasticsearch():
+def new_elasticsearch():
     from elasticsearch import Elasticsearch
+    global local_es
     if local_es is None:
         local_es = Elasticsearch(es_url)
 
     return local_es
 
 
-def get_hbase_row_value(table, row_id, key_str):
+def hbase_row_value(table, row_id, key_id):
     """
     :param str table: The name of the MEMEX HBase table
     :param str row_id: The row to get from the table
@@ -44,48 +45,26 @@ def get_hbase_row_value(table, row_id, key_str):
     """
     import requests
     try:
-        hbase_url = 'http://{}/{}/{}/{}'.format(HBASE_URL, row_id, key_str)
+        hbase_url = 'http://{}/{}/{}/{}'.format(HBASE_URL, table, row_id, key_id)
         r = requests.get(hbase_url)
         if r.status_code == 200:
             return r.text
     except:
         pass
+
     return None
 
 
-def get_first_cdr_entry_matching_dict(term_dict, es=None):
-    """
-    Get the first result matching a set of hard identifiers back frm the CDR.
-    :param dict term_dict:
-    :param elasticsearch.Elasticsearch es:
-    :param tuple auth_tuple:
-    :return dict:
-    """
-    if es is None:
-        es = get_new_elasticsearch(auth_tuple)
-    q = {
-        "filter": {
-            "bool": {
-                "must": {
-                    "term": term_dict
-                }
-            }
-        }
-    }
-    es.search(body=q)
-
-
-def get_image_stored_url(image_id, es=None):
+def image_stored_url(image_id, es=None):
     """
     Checks various MEMEX sources for the location at which a particular
     image's raw binary is stored.
     :param str image_id:
     :param elasticsearch.Elasticsearch es:
-    :param tuple auth_tuple:
     :return str:
     """
     try:
-        image_json = get_hbase_row_value('dig_isi_cdr2_ht_images',
+        image_json = hbase_row_value('dig_isi_cdr2_ht_images',
                                          image_id,
                                          'images:images')
         image_url = eval(image_json)['obj_stored_url']
@@ -93,17 +72,14 @@ def get_image_stored_url(image_id, es=None):
             return image_url
     except:
         pass
+
     try:
         # Get the url from elastic
-        # if es is None:
-        #     es = get_new_elasticsearch(auth_tuple)
-        q = get_must_bool_filter_query({'_id': image_id})
-        # r = es.get(index=ES_INDEX, doc_type='_all', id=image_id,
-        # fields=['obj_stored_url'])
-        import requests
-        es_url = es_url_fmt(auth_tuple)
-        r = requests.get(es_url, data=json.dumps(q))
-        image_url = r.json()['hits']['hits'][0]['_source']['obj_stored_url']
+        if es is None:
+             es = new_elasticsearch()
+        q = must_bool_filter_query({'_id': image_id})
+        data_dict = es.search(body=q, filter_path=['hits.hits._source'])
+        image_url = data_dict['hits']['hits'][0]['_source']['obj_stored_url']
         return image_url
     except:
         pass
@@ -111,23 +87,25 @@ def get_image_stored_url(image_id, es=None):
     return None
 
 
-def get_image_hash(image_id, es=None, auth_tuple=None):
+def image_hash(image_id, es=None):
     """
     Checks various MEMEX resources for the image hash.
     If we can't find it, calculate the hash.
     :param str image_id:
+    :param elasticsearch.Elasticsearch es:
     :returns str:
     """
     import requests
+
     # Check Svebor's hash lookup table
-    image_hash = get_hbase_row_value('ht_images_cdrid_to_sha1_2016',
+    image_hash = hbase_row_value('ht_images_cdrid_to_sha1_2016',
                                      image_id,
                                      'hash:sha1')
     if image_hash is not None:
         return image_hash
 
     # Get raw image data, then calculate the hash.
-    image_url = get_image_stored_url(image_id, es, auth_tuple)
+    image_url = image_stored_url(image_id, es)
     r = requests.get(image_url)
     data = r.text
 
@@ -136,26 +114,29 @@ def get_image_hash(image_id, es=None, auth_tuple=None):
     h.update(data.encode('utf8'))
     return h.hexdigest().upper()
 
-def get_timestamp_for_id(cdr_id):
-    """
 
-    :param str cdr_id:
+def timestamp_for_cdr_id(cdr_id, es=None):
     """
-    data_str = get_hbase_row_value('dig_isi_cdr2_ht_images',
+    Return the timestamp for this CDR ID
+    :param str cdr_id:
+    :param elasticsearch.Elasticsearch es:
+    :returns int:
+    """
+    data_str = hbase_row_value('dig_isi_cdr2_ht_images',
                                    cdr_id,
                                    'images:images')
     if data_str is not None:
         return eval(data_str)['timestamp']
 
-    data_str = get_hbase_row_value('ht_images_cdrid_to_image_ht_id',
+    data_str = hbase_row_value('ht_images_cdrid_to_image_ht_id',
                                    cdr_id,
                                    'info:timestamp')
     if data_str is not None:
         return int(data_str)
 
-    q = get_must_bool_filter_query({'_id': cdr_id})
+    q = must_bool_filter_query({'_id': cdr_id})
     if es is None:
-         es = get_new_elasticsearch(auth_tuple)
+        es = new_elasticsearch()
     data_dict = es.search(body=q, filter_path=['hits.hits._source'])
     if data_dict is not None:
         return data_dict['hits']['hits'][0]['_source']['timestamp']
--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from HelperFuncs import image_hash
 from HelperFuncs import hbase_row_value
+from HelperFuncs import image_hash
+from HelperFuncs import timestamp_for_cdr_id
 
 
-def query_one(image_id, es=None, auth_tuple=None):
+def query_one(image_id, es=None):
     """
     Goal: For a given image image_id, returns the first date (in the CDR) that it appeared)
     Currently: Get the image from the CDR
@@ -12,18 +13,17 @@ def query_one(image_id, es=None, auth_tuple=None):
     give up.
     Hit Svebor's table for a list of timestamps.
     If that doesn't work, hit elastic
-    TODO Use Lattice's extracted post data, not Svebor's table.
-    TODO Make default more efficient by hitting elastic with all ads at once
+    :todo: Use Lattice's extracted post data, not Svebor's table.
+    :todo: Make default more efficient by hitting elastic with all ads at once
     :param str image_id: The image_id to retrieve
     :param elasticsearch.Elasticsearch es: the elasticsearch index to search
-    :param tuple auth_tuple: Two-tuple of username and password.
     """
-    image_hash = image_hash(image_id, es, auth_tuple)
+    cur_hash = image_hash(image_id, es)
 
     # Check Svebor's table for a copy of the hashed image, and get the other
     # images.
     ad_ids = hbase_row_value('ht_images_infos_2016',
-                             image_hash,
+                             cur_hash,
                              'info:all_parent_ids')
 
     if ad_ids is None:
@@ -31,4 +31,3 @@ def query_one(image_id, es=None, auth_tuple=None):
         return None
 
     return min([timestamp_for_cdr_id(ad_id) for ad_id in ad_ids.split(',')])
-    min_timestamp = None
