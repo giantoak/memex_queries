fb876881e6ece7ebfcc98234defa6e9c39cd518c
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-04-23 12:01:50 -0700

    Fixed sqlite pulls for DD/CDR data, added functions.

--- memex_queries/helpers/sqlite.py
+++ memex_queries/helpers/sqlite.py
@@ -26,16 +26,18 @@ def dd_df_from_sqlite_tables(dd_ids, sqlite_tables, sql_con=None):
     """
     from pandas import read_sql
 
-    dd_ids_str = ','.join(str(x) for x in dd_ids)
+    dd_ids_str = ','.join(['"{}"'.format(x) for x in dd_ids])
     query_fmt = 'select * from {} where dd_id in ({})'.format
 
     if sql_con is None:
         sql_con = new_sqlite_con()
 
     df = read_sql(query_fmt(sqlite_tables[0], dd_ids_str), sql_con)
+    df['dd_id'] = df.dd_id.astype(int)
 
     for s_t in sqlite_tables[1:]:
         df_2 = read_sql(query_fmt(s_t, dd_ids_str), sql_con)
+        df_2['dd_id'] = df_2.dd_id.astype(int)
         df = df.merge(df_2, on=['dd_id'], how='outer')
 
     if 'post_date' in df:
@@ -43,6 +45,28 @@ def dd_df_from_sqlite_tables(dd_ids, sqlite_tables, sql_con=None):
         df['post_date'] = df.post_date.apply(to_datetime)
 
     return df
+
+
+def cdr_df_from_sqlite_tables(cdr_ad_ids, sqlite_tables, sql_con=None):
+    """
+    :param list cdr_ad_ids: list of CDR Ad IDs to match with deep dive data
+    :param list sqlite_tables: list of SQLite tables to join
+    :param sqlalchemy.create_engine sql_con: Connection to SQLite (can be \
+    omitted)
+    :returns: `pandas.DataFrame` -- dataframe of tables, joined using the Deep \
+    Dive IDs.
+    """
+    from pandas import read_sql
+
+    cdr_ids_str = ','.join(['"{}"'.format(cdr_id) for cdr_id in cdr_ad_ids])
+
+    if sql_con is None:
+        sql_con = new_sqlite_con()
+
+    df = read_sql('select * from dd_id_to_cdr_id where cdr_id in ({})'.format(cdr_ids_str), sql_con)
+    df_2 = dd_df_from_sqlite_tables(list(df.dd_id), [x for x in sqlite_tables if x != 'dd_id_to_cdr_id'], sql_con)
+
+    return df.merge(df_2, on=['dd_id'], how='outer')
 
 
 def get_phones_for_dd_ids(dd_ids, sql_con=None):
