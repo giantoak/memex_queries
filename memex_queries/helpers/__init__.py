8f3d443bbae46fb7cb31164516a44d48d2517183
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-04-23 12:03:52 -0700

    Switch some HBase calls for SQLite, fix docstrings.

--- memex_queries/helpers/__init__.py
+++ memex_queries/helpers/__init__.py
@@ -6,6 +6,7 @@ from .hbase import cdr_id_from_dd_id
 from .hbase import dd_id_from_cdr_id
 from .hbase import dd_id_df
 from .hbase import hbase_row_value
+from .sqlite import cdr_df_from_sqlite_tables
 from .sqlite import dd_df_from_sqlite_tables
 
 
@@ -27,27 +28,8 @@ def cdr_ad_ids_for_general_cdr_image_id(cdr_image_id, es=None):
     if ad_ids is not None:
         return ad_ids.split(',')
 
-    # Our fallback is to hit elastic and get every parent of the *speicific& current image
-    return cdr_ad_ids_for_cdr_image_ids(cdr_image_id, es)
-
-
-def image_stored_url(cdr_image_id, es=None):
-    """
-    :param str cdr_image_id: A CDR Image ID
-    :param elasticsearch.Elasticsearch es: CDR Connection (can be omitted)
-    :returns: `str` -- The image's stored URL, or `None`
-    """
-    try:
-        image_json = hbase_row_value('dig_isi_cdr2_ht_images',
-                                     cdr_image_id,
-                                     'images:images')
-        image_url = eval(image_json)['obj_stored_url']
-        if image_url is not None:
-            return image_url
-    except:
-        pass
-
-    return stored_url_of_cdr_image_id(cdr_image_id, es)
+    # Our fallback is to hit elastic and get every parent of the *specific* current image
+    return cdr_ad_ids_for_cdr_image_ids([cdr_image_id], es)[0]
 
 
 def image_hashes(cdr_image_ids, es=None):
@@ -113,14 +95,16 @@ def image_hash(cdr_image_id, es=None):
 
 def post_dates_for_cdr_ad_ids(cdr_ad_ids):
     """
-    Given a list of CDR Ad IDs, find their DD IDs and when they were posted.
+    Given a list of CDR IDs of advertisements, find their DD IDs and when they were posted.
 
     :param list cdr_ad_ids: A list of CDR IDs of escort ads.
     :returns: `pandas.DataFrame` -- DataFrame of CDR IDs, DD IDs, and Post Dates
     """
-    return dd_id_df(cdr_ad_ids).join(dd_df_from_sqlite_tables([dd_id_from_cdr_id(x) for x in cdr_ad_ids],
-                                                              'dd_id_to_post_date'),
-                                     on=['dd_id'])
+    return cdr_df_from_sqlite_tables(cdr_ad_ids, ['dd_id_to_post_date'])
+    # return dd_id_df(cdr_ad_ids).join(dd_df_from_sqlite_tables(
+    #     [dd_id_from_cdr_id(x) for x in cdr_ad_ids],
+    #     ['dd_id_to_post_date']
+    # ), on=['dd_id'])
 
 
 def post_dates_for_general_cdr_image_id(cdr_image_id, es=None):
@@ -141,7 +125,6 @@ def cdr_image_ids_for_dd_ad_id(dd_ad_id):
     :param int dd_ad_id: The Deep Dive ID of an escort ad.
     :returns: `list` -- The list of CDR IDs of the images used with this ad.
     """
-
     # The HBase table isn't yet fully populated, so we use sqlite
     # cdr_ad_id = hbase_row_value('deepdive_escort_ads', dd_ad_id, 'info:cdr_id')
     cdr_ad_id = dd_df_from_sqlite_tables([dd_ad_id], ['dd_id_to_cdr_id']).iloc[0, 1]
