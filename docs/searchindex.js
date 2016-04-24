51c9620976299b76ddefa6e1998073e5758f2ed9
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-04-24 15:35:26 -0400

    Regenerated docs.

--- docs/searchindex.js
+++ docs/searchindex.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7673d7b72ec2d8ab670a7552a9d7c158cb3edffd3ca46893f4ddf4bb6960434
-size 4702
+oid sha256:6f1d7b1f465b49353d7e84582c9b7facfa1f00ac7f92498d9c40912349416deb
+size 5364
